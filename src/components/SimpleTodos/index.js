c42f85a3d3f106184636cbf0b3188c52b53e43e8
Jaswanth-Kollipara <kolliparajaswanth5@gmail.com>
2024-06-12 06:49:09 +0000

    first commit

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,47 +1,16 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 import TodoItem from '../TodoItem'
 
 import './index.css'
 
-const initialTodosList = [
-  {
-    id: 1,
-    title: 'Book the ticket for today evening',
-  },
-  {
-    id: 2,
-    title: 'Rent the movie for tomorrow movie night',
-  },
-  {
-    id: 3,
-    title: 'Confirm the slot for the yoga session tomorrow morning',
-  },
-  {
-    id: 4,
-    title: 'Drop the parcel at Bloomingdale',
-  },
-  {
-    id: 5,
-    title: 'Order fruits on Big Basket',
-  },
-  {
-    id: 6,
-    title: 'Fix the production issue',
-  },
-  {
-    id: 7,
-    title: 'Confirm my slot for Saturday Night',
-  },
-  {
-    id: 8,
-    title: 'Get essentials for Sunday car wash',
-  },
-]
+const isNumeric = string => Number.isFinite(+string)
 
 class SimpleTodos extends Component {
   state = {
-    todosList: initialTodosList,
+    todosList: [],
+    text: '',
   }
 
   deleteTodo = id => {
@@ -53,6 +22,76 @@ class SimpleTodos extends Component {
     })
   }
 
+  onChangeInput = event => {
+    this.setState({text: event.target.value})
+  }
+
+  onClickAdd = () => {
+    const {text} = this.state
+    const textArray = text.split(' ')
+    if (isNumeric(textArray[textArray.length - 1])) {
+      let updatedText = ''
+      for (let i = 0; i < textArray.length - 1; i += 1) {
+        updatedText += `${textArray[i]} `
+      }
+      const times = parseInt(textArray[textArray.length - 1])
+      let val = []
+      for (let i = 0; i < times; i += 1) {
+        const val1 = {
+          id: uuidv4(),
+          title: updatedText,
+          isChecked: false,
+          isSaved: true,
+        }
+        val = [...val, val1]
+      }
+      this.setState(prevState => ({
+        todosList: [...prevState.todosList, ...val],
+      }))
+    } else {
+      const val = {
+        id: uuidv4(),
+        title: text,
+        isChecked: false,
+        isSaved: true,
+      }
+      this.setState(prevState => ({todosList: [...prevState.todosList, val]}))
+    }
+  }
+
+  changeChecked = id => {
+    this.setState(prevState => ({
+      todosList: prevState.todosList.map(eachContact => {
+        if (id === eachContact.id) {
+          return {...eachContact, isChecked: !eachContact.isChecked}
+        }
+        return eachContact
+      }),
+    }))
+  }
+
+  changeSaved = id => {
+    this.setState(prevState => ({
+      todosList: prevState.todosList.map(eachContact => {
+        if (id === eachContact.id) {
+          return {...eachContact, isSaved: !eachContact.isSaved}
+        }
+        return eachContact
+      }),
+    }))
+  }
+
+  changeTodoText = (text, id) => {
+    this.setState(prevState => ({
+      todosList: prevState.todosList.map(eachContact => {
+        if (id === eachContact.id) {
+          return {...eachContact, title: text}
+        }
+        return eachContact
+      }),
+    }))
+  }
+
   render() {
     const {todosList} = this.state
 
@@ -60,12 +99,24 @@ class SimpleTodos extends Component {
       <div className="app-container">
         <div className="simple-todos-container">
           <h1 className="heading">Simple Todos</h1>
+          <div>
+            <input
+              className="simple-todos-input"
+              onChange={this.onChangeInput}
+            />
+            <button type="button" onClick={this.onClickAdd}>
+              Add
+            </button>
+          </div>
           <ul className="todos-list">
             {todosList.map(eachTodo => (
               <TodoItem
                 key={eachTodo.id}
                 todoDetails={eachTodo}
                 deleteTodo={this.deleteTodo}
+                changeChecked={this.changeChecked}
+                changeSaved={this.changeSaved}
+                changeTodoText={this.changeTodoText}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,19 +1,56 @@
 import './index.css'
 
 const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {id, title} = todoDetails
+  const {
+    todoDetails,
+    deleteTodo,
+    changeChecked,
+    changeSaved,
+    changeTodoText,
+  } = props
+  const {id, title, isChecked, isSaved} = todoDetails
 
   const onDeleteTodo = () => {
     deleteTodo(id)
   }
 
+  const onClickChecked = () => {
+    changeChecked(id)
+  }
+
+  const onEditTodo = () => {
+    changeSaved(id)
+  }
+
+  const onChangeTodoText = event => {
+    changeTodoText(event.target.value, id)
+  }
+
   return (
     <li className="todo-item">
-      <p className="title">{title}</p>
-      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
-        Delete
-      </button>
+      <input type="checkbox" checked={isChecked} onClick={onClickChecked} />
+      {isSaved && (
+        <>
+          <p className="title">{title}</p>
+          <button type="button" className="edit-btn" onClick={onEditTodo}>
+            Edit
+          </button>
+          <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+            Delete
+          </button>
+        </>
+      )}
+      {!isSaved && (
+        <>
+          <input className="title" value={title} onChange={onChangeTodoText} />
+          <button type="button" className="save-btn" onClick={onEditTodo}>
+            Save
+          </button>
+          <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+            Delete
+          </button>
+        </>
+      )}
     </li>
   )
 }
